0586b5cc2cf638ce824c8ae4b9b2742d70a89e3e
Ravil Gataullin <ravil.gataullin@kaspersky.com>
2021-04-03 16:43:04 +0300

    Initialize dockerfile

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request
 from flask_caching import Cache
 from werkzeug.exceptions import BadRequest
@@ -36,6 +38,5 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-
-    app.env = 'development'  # TODO: production
-    app.run(host='127.0.0.1', debug=True)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host="0.0.0.0", port=port)
